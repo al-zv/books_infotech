4a51a4bfe3ac93e67d6507a110eb7d1c19a8f59b
al-zv <al3z1v@gmail.com>
2026-09-18 18:11:29 +0700

    Connecting database MySQL

--- config/db.php
+++ config/db.php
@@ -2,13 +2,8 @@
 
 return [
     'class' => \yii\db\Connection::class,
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=db;dbname=books_infotech',
     'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+    'password' => 'root',
+    'charset' => 'utf8mb4',
 ];
